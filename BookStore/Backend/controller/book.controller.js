2f046dddf4b629afce03733e040f83fb96c5a3b1
Devashish Soni <devashishsoni926@gmail.com>
2025-11-17 21:44:19 +0530

    add

--- BookStore/Backend/controller/book.controller.js
+++ BookStore/Backend/controller/book.controller.js
@@ -1,9 +1,25 @@
 import Book from "../model/book.model.js";
 
+// export const getBook = async(req, res) => {
+
+//     try {
+//         const book = await Book.find();
+//         res.status(200).json(book);
+//     } catch (error) {
+//         console.log("Error: ", error);
+//         res.status(500).json(error);
+//     }
+// };
+
 export const getBook = async(req, res) => {
     try {
-        const book = await Book.find();
-        res.status(200).json(book);
+        const books = await Book.find();
+        // Map books to include numeric id based on list.json IDs
+        const booksWithNumericId = books.map(book => ({
+            ...book.toObject(),
+            id: book.id || book._id.toString()
+        }));
+        res.status(200).json(booksWithNumericId);
     } catch (error) {
         console.log("Error: ", error);
         res.status(500).json(error);
